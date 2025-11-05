fa104a2baebc38c05cf07dc93af11b475c8ef372
wenhe <zhentaogo@gmail.com>
2025-11-05 15:29:29 +0800

    feat: 更改c显示

--- c.sh
+++ c.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
 
 # 默认股票代码
-DEFAULT_SYMBOL="sh603256"
+DEFAULT_SYMBOL="sz301526,sz300490,sz002759"
 
 # 如果没有传入参数，就用默认值
 SYMBOL=${1:-$DEFAULT_SYMBOL}
 
-URL="http://localhost:3000/quote?symbol=${SYMBOL}"
+URL="http://localhost:3000/cli?symbol=${SYMBOL}"
 
-# Enter fullscreen
+# 进入全屏模式并隐藏光标
 tput smcup
-clear
-
-# Hide cursor
 tput civis
 
-# Restore cursor and exit fullscreen on exit
+# 退出时恢复终端状态
 trap "tput cnorm; tput rmcup" EXIT
 
+# 循环刷新
 while true; do
-  CHANGE=$(curl -s "$URL" | jq -r '.changeRate')
-  # 动态刷新（同一行更新）
-  # echo -ne "(${SYMBOL}) : ${CHANGE}    \r"
-  echo -ne "${CHANGE}    \r"
-  sleep 1
+  clear
+  echo "$(date '+%H:%M:%S')"
+  curl -s "$URL"
+  sleep 10
 done
