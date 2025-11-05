08e0045549c900e84b8a2785569e2d7beb7eaf4b
wenhe <zhentaogo@gmail.com>
2025-11-05 16:03:48 +0800

    feat: 优化一下c.sh

--- c.sh
+++ c.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
 # 默认股票代码
-DEFAULT_SYMBOL="sz301526,sz300490,sz002759"
-
+DEFAULT_SYMBOL="sh601138,sz300490,sz002759"
 # 如果没有传入参数，就用默认值
 SYMBOL=${1:-$DEFAULT_SYMBOL}
-
 URL="http://localhost:3000/cli?symbol=${SYMBOL}"
 
-# 进入全屏模式并隐藏光标
+# 保存并进入全屏模式，隐藏光标
 tput smcup
 tput civis
 
 # 退出时恢复终端状态
 trap "tput cnorm; tput rmcup" EXIT
 
-# 循环刷新
+# 清空一次并固定光标在顶部
+clear
+tput cup 0 0
+
 while true; do
-  clear
+  # 回到顶部覆盖输出（不清屏）
+  tput cup 0 0
   echo "$(date '+%H:%M:%S')"
   curl -s "$URL"
-  sleep 10
+  sleep 1
 done
